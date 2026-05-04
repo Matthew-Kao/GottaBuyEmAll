8bbf5d50283a940e277cbdc181e4ea8d1ba07c94
Matthew Kao <matthewkao@berkeley.edu>
2026-05-04 17:01:53 +0700

    google redirect

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -108,7 +108,7 @@ async def logout():
 
 @router.get("/google")
 async def google_login(request: Request):
-    redirect_uri = str(request.url_for("google_callback")) 
+    redirect_uri = "https://gottabuyemall-production.up.railway.app/auth/google/callback" 
     return await oauth.google.authorize_redirect(request, redirect_uri)
 
 
